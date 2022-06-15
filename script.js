c97594fd50a0c2b2800c72ddd1a973801363f6c2
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-15 15:21:06 -0500

    Implement a grid based on a grid size

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const container = document.getElementById('container');
+const container = document.getElementsByClassName('container');
 
 
-const gridSize = 5;
+const gridSize = 10;
 
-for (i=0; i < gridSize; i++) {
-    const gridCell = document.createElement('div');
-    gridCell.classList.add('cell');
-    container.appendChild(gridCell);
+function makeBlocks() {
+    for (let i = 0; i < gridSize; i++) { //Creates the rows
+        const row = document.createElement('div');
+        row.className = "row";
+        for (let j = 0; j < gridSize; j++) {
+            const cell = document.createElement('div'); //Creates cells in row
+            cell.className = "cell";
+            row.appendChild(cell);
+        }                
+        container[0].appendChild(row);
+    }
 }
+
+makeBlocks();
