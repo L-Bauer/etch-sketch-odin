328d903167f46ccedb46c417d417a7011a9245ea
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-15 15:28:50 -0500

    Setup grid location and size
    The grid now stays centered and will change
    the cells width based on the number of columns.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const container = document.getElementsByClassName('container');
 
 
-const gridSize = 10;
+const gridSize = 64;
 
 function makeBlocks() {
     for (let i = 0; i < gridSize; i++) { //Creates the rows
-        const row = document.createElement('div');
-        row.className = "row";
+        const col = document.createElement('div');
+        col.className = "col";
         for (let j = 0; j < gridSize; j++) {
             const cell = document.createElement('div'); //Creates cells in row
             cell.className = "cell";
-            row.appendChild(cell);
+            col.appendChild(cell);
         }                
-        container[0].appendChild(row);
+        container[0].appendChild(col);
     }
 }
 
