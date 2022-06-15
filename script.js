8d8cefb30447cb0286504e8235c02fb0affdfeb1
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-15 14:22:11 -0500

    Add a row of cells
    Creates a row of cells inside the container

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.getElementById('container');
-const gridCell = document.createElement('div');
 
-gridCell.classList.add('cell');
 
-container.appendChild(gridCell);
+const gridSize = 5;
+
+for (i=0; i < gridSize; i++) {
+    const gridCell = document.createElement('div');
+    gridCell.classList.add('cell');
+    container.appendChild(gridCell);
+}
