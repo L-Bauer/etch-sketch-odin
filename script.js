38c578e69420330276afcaa321f6aac66a3e914d
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-17 09:32:52 -0500

    Setup grid using css grid layout

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
 
 
-const containerBox = document.querySelector('.container');
-const gridCell = document.getElementsByClassName('cell'); 
-const gridSize = 25;
+const grid = document.querySelector('.grid');
+const gridCell = document.querySelectorAll('cell'); 
+const gridSize = 16;
 let isToggling = false;
 
-window.onload = makeBlocks(), colorCells();
 
-//Makes the grid
-function makeBlocks() {
-    for (let i = 0; i < gridSize; i++) { //Creates the rows
-        const col = document.createElement('div');
-        col.className = "col";
-        for (let j = 0; j < gridSize; j++) {
-            const cell = document.createElement('div'); //Creates cells in row
-            cell.className = "cell";
-            col.appendChild(cell);
-        }                
-        containerBox.appendChild(col);
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let totalGridSize = size**2;
+
+    for (let i = 0; i < totalGridSize; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.className ='cell';
+        grid.appendChild(gridElement);
     }
 }
 
-// //Creates event to listen on the "cells". Looking for if the cells have the 
-// //mouse down event
 
-// function enableToggle(e) {
-//     if (e.target.classList == 'cell' || e.target.classList == 'cell active') {
-//         console.log('enable', e.target);
-//         e.target.classList.toggle('active');
-//     }
-//     isToggling = true;
-//     console.log('enableToggle');
-// }
+//Creates event to listen on the "cells". Looking for if the cells have the 
+//mouse down event
 
-// function disableToggle() {
-//     console.log('disableToggle');
-//     isToggling = false;
-// }
+function enableToggle(e) {
+    if (e.target.classList == 'cell' || e.target.classList == 'cell active') {
+        console.log('enable', e.target);
+        e.target.classList.toggle('active');
+    }
+    isToggling = true;
+    console.log('enableToggle');
+}
 
-// function toggle(e) {
-//     if (isToggling === false) {
-//         return;
-//     }
-//     console.log('toggle:', e.target);
-//     e.target.classList.toggle('active');
-// }
+function disableToggle() {
+    console.log('disableToggle');
+    isToggling = false;
+}
 
-// function colorCells() {
-//     document.addEventListener('mousedown', enableToggle);
+function toggle(e) {
+    if (isToggling === false) {
+        return;
+    }
+    console.log('toggle:', e.target);
+    e.target.classList.toggle('active');
+}
+
+function colorCells() {
+    document.addEventListener('mousedown', enableToggle);
   
-//     for (let i = 0, il = gridCell.length; i < il; i++) {
-//         gridCell[i].addEventListener('mouseenter', toggle);
-//     }
+    for (let i = 0, il = gridCell.length; i < il; i++) {
+        gridCell[i].addEventListener('mouseover', toggle);
+    }
 
-//     document.addEventListener('mouseup', disableToggle);
-// }
+    document.addEventListener('mouseup', disableToggle);
+}
+
+colorCells();
